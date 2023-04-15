9d8e045ec88240897faca7fbd25f855dc40c89ab
mercyplaisir <kirangimercyplaisir@gmail.com>
2023-04-15 10:40:36 +0300

    add profile page

--- foodreview/user/models.py
+++ foodreview/user/models.py
@@ -1,5 +1,25 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
-from django.contrib.auth.models import User
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from .managers import CustomUserManager
 
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    first_name  = models.CharField(_("first name"), max_length=50,null=True)
+    last_name = models.CharField(_("last name"), max_length=50,null=True)
+    d_o_b = models.DateField(null=True)
+    phone_number = models.CharField(_("phone number"),max_length=30,null=True)
+    email = models.EmailField(_("email address"), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
--- foodreview/user/views.py
+++ foodreview/user/views.py
@@ -1,7 +1,7 @@
 import random
 import string
 from django.shortcuts import render
-from .forms import SignupForm
+from .forms import CustomerSignupForm,CustomerChangeForm
 # Create your views here.
 
 def login_page(response):
@@ -9,14 +9,19 @@ def login_page(response):
 
 def signup_page(response):
     if response.method == "POST":
-        form = SignupForm(response.POST)
+        form = CustomerSignupForm(response.POST)
         if form.is_valid():
             
             letters = string.ascii_lowercase
             user_n =  ''.join(random.choice(letters) for i in range(10))
-            form.cleaned_data['username'] = user_n
+            
             print(form.data)
-            form
+            form.save()
     else:
-        form= SignupForm()
+        form= CustomerSignupForm()
     return render(response,"user/signup.html",{'form':form})
+
+def profile(response):
+    user = response.user
+    form = CustomerChangeForm(instance=user)
+    return render(response,'user/profile.html',{'form':form})
