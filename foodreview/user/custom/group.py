5f77ae41eedbf4f488ef3f3f4b739595af1e433b
mercyplaisir <kirangimercyplaisir@gmail.com>
2023-06-04 15:32:43 +0300

    refactoring

--- foodreview/user/custom/group.py
+++ foodreview/user/custom/group.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
 from restaurant.models import Restaurant
-from enum import StrEnum,auto
 from django.db import models
 
 from django.db.utils import IntegrityError,ProgrammingError
